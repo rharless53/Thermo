deb6ee068dcf9023bec140848a28f716ab89283c
rharless53 <r.harless53@gmail.com>
2026-07-15 13:45:19 -0400

    Added point increasing functionality

--- dT_Optimizer.m
+++ dT_Optimizer.m
@@ -2,44 +2,51 @@
 % AUTHOR: Richard Harless
 % DATE: 7/14/2026
 
-function [X,Y,MaxDiff,n] = dT_Optimizer(L_Bound,U_Bound,Function)
+function [X,Y,MaxDiff,n] = dT_Optimizer(L_Bound,U_Bound,Function,max_err)
 
 %Create Points
-n = ceil(2.2*(U_Bound-L_Bound));
+n = 10;
 too_much_error = 0;
+iterations = 0;
+increasing = 0;
 
 while too_much_error == 0
     Points = linspace(L_Bound,U_Bound,n);
-    relax = 0.1;
+    relax = 0.01*(U_Bound-L_Bound);
     Max_dT = zeros(n-1,1);
-    % Graph = plot(Points,Function(Points),'k.','MarkerSize',10);
-    % hold on
-    % X1 = linspace(L_Bound,U_Bound,100);
-    % plot(X1,Function(X1),'b')
+    Graph = plot(Points,Function(Points),'k.','MarkerSize',10);
+    hold on
+    X1 = linspace(L_Bound,U_Bound,100);
+    plot(X1,Function(X1),'b')
     D = 10*ones(n-1,1);
-    while max(abs(D)) > 1e-3
-
+    while max(abs(D)) > 1e-2
         for i = 1:n-1
             Max_dT(i) = dT(Points(i),Points(i+1),Function);
         end
-
         D = gradient(Max_dT);
         Points(2:n-1) = Points(2:n-1) + transpose(relax*D(2:n-1));
-        % Graph.XData = Points;
-        % Graph.YData = Function(Points);
-        % drawnow
+        Graph.XData = Points;
+        Graph.YData = Function(Points);
+        drawnow
     end
-    % hold off
+    hold off
 
-    if max(Max_dT) < 1
+    if max(Max_dT) < max_err && n ~= 2
         n = n-1;
+        increasing = 0;
+    elseif max(Max_dT) > max_err && (iterations == 0 || increasing == 1)
+        n = n + 1;
+        increasing = 1;
     else
         too_much_error = 1;
     end
+
+    iterations = iterations + 1;
 end
-X = Points
-Y = Function(Points)
-MaxDiff = max(Max_dT)
+
+X = Points;
+Y = Function(Points);
+MaxDiff = max(Max_dT);
 
     function max_dT_percent = dT(A,B,Function)
 
